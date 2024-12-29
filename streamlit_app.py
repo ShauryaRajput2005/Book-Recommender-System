81f234c0c62cd9dc449e6138fbecb37c7d5a2f66
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2024-12-29 21:04:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+Top_page=st.Page("C:\\Users\\Asus\\Downloads\\MLProjects\\book_rec\\page1.py",title='Top Book📖 Recommendations', icon='📔') 
+personal=st.Page("C:\\Users\\Asus\\Downloads\\MLProjects\\book_rec\\page2.py",title='Get Personalised Recommendations', icon='🧑‍🎓')
 
-st.write('Hello world!')
+
+pg=st.navigation([Top_page,personal])
+st.set_page_config(page_title='Top Recommendations',page_icon='📚',layout='wide')
+pg.run()
