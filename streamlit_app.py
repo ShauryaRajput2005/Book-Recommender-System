fcce9933a9150c6228a8467578e67e37aba9250a
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2024-12-30 20:02:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 Top_page=st.Page("page1.py",title='Top Book📖 Recommendations', icon='📔') 
 personal=st.Page("page2.py",title='Get Personalised Recommendations', icon='🧑‍🎓')
