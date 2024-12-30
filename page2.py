af11d32f417fd91aa2613e639f0acf1924a7c0b7
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2024-12-30 20:03:11 +0530

    Update page2.py

--- page2.py
+++ page2.py
@@ -3,7 +3,14 @@ import pickle
 import pandas as pd
 import numpy as np
 
-
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 # Styling
 st.markdown(
     """
